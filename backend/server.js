f6c05b384dfe1ce4785555a5f95b2e72b533ef07
vedanttttt <51362126+vedanttttt@users.noreply.github.com>
2026-02-19 19:07:25 +0530

    new changes for deplpoyment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 
@@ -10,6 +11,12 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
 
 // Routes
 const urlRoutes = require("./routes/urlRoutes");
